53a9d0b0eed9db99522cb018139d60c27663fbf0
unknown <dartmol21@mail.ru>
2026-08-06 01:58:47 +0300

    update env for time interval

--- vk-bot/app/config.py
+++ vk-bot/app/config.py
@@ -7,5 +7,5 @@ class Settings(BaseSettings):
     vk_token: str
     api_base: str = "http://localhost:8000"
 
-    chat_broadcast_interval_seconds: int = 86400
+    chat_broadcast_interval_seconds: int = 3600
     wall_post_interval_seconds: int = 600
